b55f0395ac513c5ebb455809908c7199984cb4cf
lethihangtk15 <70497474+lethihangtk15@users.noreply.github.com>
2020-10-12 00:41:45 +0700

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,47 @@
+import { LoginComponent } from './customer/login/login.component';
+import { ChitietComponent } from './product/chitiet/chitiet.component';
+import { MainComponent } from './main/main.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NotFoundComponent } from './shared/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'product',
+    loadChildren: () => import('./product/product.module').then((m) => m.ProductModule),
+  },
+  // {
+  //   path: 'customer',
+  //   loadChildren: () => import('./customer/customer.module').then((m) => m.CustomerModule),
+  // },
+  {
+    path: 'home',
+    loadChildren: () => import('./main/main.module').then((m) => m.MainModule),
+  },
+  {
+    path: '',
+    component: MainComponent,
+  },
+  {
+    path: 'home',
+    component: MainComponent,
+  },
+  {
+    path: 'chitiet',
+    component: ChitietComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  { path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/main/main.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    // SharedModule,
+    // RouterModule.forChild(routes),
+  ]
+})
+export class MainModule { }
