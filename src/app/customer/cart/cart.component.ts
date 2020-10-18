5ac041add89870608795b850998c0adfc6eaf683
lethihangtk15 <70497474+lethihangtk15@users.noreply.github.com>
2020-10-19 00:57:11 +0700

    update

--- src/app/customer/cart/cart.component.ts
+++ src/app/customer/cart/cart.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { BaseComponent } from './../../lib/base-component';
+import { Component, OnInit, Injector } from '@angular/core';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
-export class CartComponent implements OnInit {
+export class CartComponent extends BaseComponent implements OnInit {
+  items:any;
+  total:any;
 
-  constructor() { }
+  constructor(injector: Injector) {
+    super(injector);
+   }
 
   ngOnInit(): void {
+    this._cart.items.subscribe((res) => {
+      this.items = res;
+      this.total = 0;
+      for(let x of this.items){
+        x.money = x.quantity * x.product_price;
+        this.total += x.quantity * x.product_price;
+      }
+    });
+  }
+
+  clearCart() {
+    this._cart.clearCart();
+    alert('Xóa thành công');
+  }
+
+  addQty(item, quantity){
+    item.quantity =  quantity;
+    item.money =  Number.parseInt(item.quantity) *  item.product_price;
+    this._cart.addQty(item);
   }
 
 }
